dce3440b1dd392cfcf570354ccfabd5642bda6c6
charlesmanna <charlesjmanna@gmail.com>
2020-01-18 11:09:04 -0800

    added project files

---
+++ Assets/Scripts/Enemy/EnemyData.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class EnemyData : MonoBehaviour{
+
+    public GameObject prefab;
+    public float minSpawnDelay;  // number of seconds between spawns at hardest difficulty
+    public float maxSpawnDelay;  // number of seconds between spawns at the start
+    public float timeUntilMaxSpawnDelay; // number of seconds between enemy beginning to spawn and the enemy spawning at its maximum rate
+    public float firstSpawnTime;     // How many seconds to wait before this enemy type spawns
+    public int score;  // points given when enemy spawns
+
+    [HideInInspector]
+    public float spawnTimer;
+    [HideInInspector]
+    public float currentSpawnDelay;
+
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+
+    public EnemyData[] enemySpawns;
+    int score = 0;
+    public Text gameover;
+
+    void Update () {
+        for ( int i = 0; i < enemySpawns.Length; i++) {
+            if (enemySpawns[i] != null) // Check to see if this GameObject exists before attempting to use its variables
+            {
+                enemySpawns[i].firstSpawnTime -= Time.deltaTime;
+                if (enemySpawns[i].firstSpawnTime > 0) // Wait firstSpawnTime seconds before you spawn this enemy type
+                {
+                    continue;
+                }
+
+                float lerpX = Mathf.Abs(enemySpawns[i].firstSpawnTime) / enemySpawns[i].timeUntilMaxSpawnDelay;
+                lerpX = Mathf.Clamp(lerpX, 0.0f, 1.0f);
+                enemySpawns[i].currentSpawnDelay = Mathf.Lerp(enemySpawns[i].maxSpawnDelay, enemySpawns[i].minSpawnDelay, lerpX);
+                enemySpawns[i].spawnTimer -= Time.deltaTime;
+                if (enemySpawns[i].spawnTimer <= 0)
+                {
+                    enemySpawns[i].spawnTimer = enemySpawns[i].currentSpawnDelay;
+                    Instantiate(enemySpawns[i].prefab);
+                    score += enemySpawns[i].score;
+                    Debug.Log("Score: " + score);
+                }
+            }
+        }
+	}
+
+    public void GameOver()
+    {
+        gameover.text = "You lose!";
+    }
+}
